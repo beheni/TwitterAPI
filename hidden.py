a8950037c384efa33b1b32b9401d94d4f31e25c1
Anastasiia Beheni <91615487+beheni@users.noreply.github.com>
2022-02-18 10:25:52 +0200

    deleted keys

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "UZiRdh7XZXb5TWmRHuvU00WL9", 
-"consumer_secret": "yd9J5MDUQ0E42L5SXIWrienhNmcIPTmpf5kRfMiY2idEu77oXI",
-"token_key": "1493865101501382661-7KXlQn6jwrNc1NftQ7hwyTgyimHFZA",
-"token_secret": "c4pzGqQfhan69NFeieqwl6KsmQdZUMKSg4AVDVZzsUum2"}
+    return {"consumer_key": "<insert your key>", 
+"consumer_secret": "<insert your key>",
+"token_key": "<insert your key>",
+"token_secret": "<insert your key>"}
