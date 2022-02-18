8ddef4b413b8a565ea1800df9eadb958933209a2
Anastasiia Beheni <91615487+beheni@users.noreply.github.com>
2022-02-18 10:17:16 +0200

    returns dictionary

---
+++ twitter2.py
@@ -0,0 +1,29 @@
+import urllib.request, urllib.parse, urllib.error
+import twurl
+import json
+import ssl
+
+# https://apps.twitter.com/
+# Create App and get the four strings, put them in hidden.py
+
+TWITTER_URL = 'https://api.twitter.com/1.1/friends/list.json'
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+
+def create_json(account: str):
+    url = twurl.augment(TWITTER_URL,
+                        {'screen_name': account})
+    connection = urllib.request.urlopen(url, context=ctx)
+    data = connection.read().decode()
+    js = json.loads(data)
+
+    # with open("twitter2.json", "w") as f:
+    #     json.dump(js, f, ensure_ascii=False, indent=4)
+    
+    return js
+
+# print(create_json("demchvk"))
