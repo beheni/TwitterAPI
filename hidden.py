f023a185b63f16514f8c10be82e6e01bcbd462b1
Anastasiia Beheni <91615487+beheni@users.noreply.github.com>
2022-02-18 10:24:56 +0200

    auxiliary file

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "UZiRdh7XZXb5TWmRHuvU00WL9", 
+"consumer_secret": "yd9J5MDUQ0E42L5SXIWrienhNmcIPTmpf5kRfMiY2idEu77oXI",
+"token_key": "1493865101501382661-7KXlQn6jwrNc1NftQ7hwyTgyimHFZA",
+"token_secret": "c4pzGqQfhan69NFeieqwl6KsmQdZUMKSg4AVDVZzsUum2"}
